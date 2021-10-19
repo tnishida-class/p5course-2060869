a36fa3181ba81a569a6bdc0695381ec79451811a
2060869 <92250172+2060869@users.noreply.github.com>
2021-10-19 11:24:53 +0900

    Change 1-q1 10/19

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -1,6 +1,6 @@
 // 練習問題：神戸市のマーク
 function setup(){
-  createCanvas(600, 200);
+  createCanvas(400, 200);
   background(255);
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
@@ -8,7 +8,7 @@ function setup(){
   strokeWeight(25);
   strokeCap(SQUARE);
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  arc(100-25, 100, 100, 100, QUARTER_PI+ PI,QUARTER_PI);
+  arc(100-25, 100, 100, 100, QUARTER_PI*5,QUARTER_PI*5+PI);
   // BLANK[1]
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
@@ -17,19 +17,21 @@ function setup(){
   const green = color(22, 131, 46);
   const gray = color(77);
 
-noFill();
-stroke(14,47,146);
-strokeWeight(25);
-ellipse(400,100,150,150);
+fill(blue);
+noStroke();
+ellipse(300,100,100,100);
+fill(255);
+ellipse(293,99,93,93);
+fill(green);
+ellipse(293,100,86,86);
+fill(255);
+ellipse(293,105,83,83);
 
-noFill();
-stroke(22,131,46);
-strokeWeight(25);
-ellipse(395,95,135,135);
-
-noFill();
-stroke(255,255,255);
-strokeWeight(25);
-ellipse(395,105,115,115);
-
+fill(gray);
+textSize(20);
+textFont("georgia");
+text('KOBE',260,100);
+fill(0,0,0);
+textSize(10);
+text('UNIVERSITY',270,115);
 }
