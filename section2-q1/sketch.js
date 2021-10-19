d0edd2caa83c9e51e92076a0600de850f6b0b0c2
2060869 <92250172+2060869@users.noreply.github.com>
2021-10-19 11:38:06 +0900

    Change 2-q1 10/19

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
+  createCanvas(200,200);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  if(i<5){  // BLANK[1]
+    noFill();
+    stroke(0,0,255);
+    ellipse(100,100,i*10+20);
   }
+  else{
+    noFill();
+    stroke(255,0,0);
+    ellipse(100,100,i*10+20);
+  }
+}
 }
