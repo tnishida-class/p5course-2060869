e963c6ecab22b8844b97d161184fa95b05db5a4e
2060869 <92250172+2060869@users.noreply.github.com>
2021-10-24 22:48:04 +0900

    section2-q2 10.24.

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,29 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if((i+j)%2==0){
+       fill(255);
+       rect(i*size,j*size,size,size);
+     }
+      else{
+        fill(77);
+        rect(i*size,j*size,size,size);
+
+      if(j<3){
+        if((i+j)%2==1){
+        fill(255,0,0);
+        ellipse(size/2+25*i,size*j+size/2,20);
+      }
+    }
+    else if(j>4){
+      if((i+j)%2==1){
+        fill(0);
+        ellipse(size/2+25*i,size*j+size/2,20);
+      }
     }
   }
+
+
+  }
+}
 }
